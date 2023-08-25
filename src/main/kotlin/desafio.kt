71cbbc4b84e35cf8e379c4b37c2d4570505b9b5a
Rodrigo Marques Dias <rodrigo.dias60@etec.sp.gov.br>
2023-08-25 04:23:43 -0300

    Aprendendo utilizar o IntelliJ IDEA

---
+++ src/main/kotlin/desafio.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var nome = "Rodrigo"
+    nome = "Rodrigo Marques Dias"
+
+    println(nome)
+
+    var textoNulo: String = ""
+
+    val sapato: Short = 39
+
+    println("eu calço $sapato")
+
+    var pib = (1869000000000)
+
+    val popBr = 201000000
+
+    println("O pib do Brasil é: $pib")
+}
